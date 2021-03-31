9fc72d374221de048bf7b57e8ec83bee1daf08c7
RyanJimeno <70073261+RyanJimeno@users.noreply.github.com>
2021-03-31 16:36:10 +0800

    gotta figure out the content jumping

--- color-flipper/index.js
+++ color-flipper/index.js
@@ -2,6 +2,10 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.querySelector("#btn")
 const color = document.querySelector(".hexcode")
 
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
+
 btn.addEventListener("click", function() {
     let hexColor = "#";
     for(let i=0; i<6; i++){
@@ -12,8 +16,5 @@ btn.addEventListener("click", function() {
     document.body.style.backgroundColor = hexColor;
 });
 
-function getRandomNumber() {
-    return Math.floor(Math.random() * hex.length);
-}
 
 // Tutorial for the JS part: https://www.youtube.com/watch?v=T4Vy92qS6KI
